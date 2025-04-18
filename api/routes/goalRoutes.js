0c7db70bd44aac5b6c31401e3d7a0e51eebb1432
aunimhsn <aunim.hassan@gmail.com>
2025-04-18 15:06:37 +0200

    feat: goalroutes updated

--- api/routes/goalRoutes.js
+++ api/routes/goalRoutes.js
@@ -1,20 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const {
+    getGoals,
+    addGoal,
+    updateGoal,
+    deleteGoal
+} = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Get goals' })
-})
-
-router.post('/', (req, res) => {
-    res.json({ message: 'Add goal' })
-})
-
-router.put('/:id', (req, res) => {
-    res.json({ message: `Update goal ${req.params.id}` })
-})
-
-router.delete('/:id', (req, res) => {
-    res.json({ message: `Delete goal ${req.params.id}` })
-})
+router.get('/', getGoals)
+router.post('/', addGoal)
+router.put('/:id', updateGoal)
+router.delete('/:id', deleteGoal)
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 5000
 // Initialisation d'Express
 const app = express()
 
+// Accepter les données envoyées par formulaire
+app.use(express.json())
+app.use(express.urlencoded())
+
 // Routes
 app.use('/api/goals', require('./routes/goalRoutes'))
 
